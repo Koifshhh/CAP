eff3edb9f1cacd6711a4bdf902c17bf60c70b708
Armani Rogers <shadowninja527@gmail.com>
2021-05-14 14:41:07 -0500

    Create dynamicData.js

---
+++ dynamicData.js
@@ -0,0 +1,6 @@
+var d = new Date();
+var strDate =  (d.getMonth()+1) + "-" + d.getDate()+ "-" + d.getFullYear();
+var sName = 'Armani Rogers';
+var sUserName = 'arogers21';
+var sUserId = '100412832';
+var aSymptoms = ['Chills','Repeated shaking with chills','Muscle pain','Headache','','Shortness of breath','Unusual fatigue','Abdominal pain','Nausea','Diarrhea','Pink eye','','','','','','','','Sudden loss of taste or smell','Dry cough'];
